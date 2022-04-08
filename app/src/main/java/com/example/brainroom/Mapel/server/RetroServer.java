31384998e6aa2a6df4d322306f28196e3a51850b
“Gilbram09” <chitoschefron2@gmail.com>
2022-04-08 09:48:17 +0700

    update dulu napa baru pada commit

--- app/src/main/java/com/example/brainroom/Mapel/server/RetroServer.java
+++ app/src/main/java/com/example/brainroom/Mapel/server/RetroServer.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
-    private static final String baseURL = "http://10.0.2.2/mapel/";
+    private static final String baseURL = "http://192.168.20.112/mapel/";
     private static Retrofit retrofit;
 
     public static Retrofit konekRetrofit() {
